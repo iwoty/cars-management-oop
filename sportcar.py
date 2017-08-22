afe52329b27dd962e12cbc73c237a69218853849
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:40:30 +0200

    Add Sportcar class constructor (with super metod)

--- sportcar.py
+++ sportcar.py
@@ -3,3 +3,8 @@ from vehicle import Vehicle
 
 
 class Sportcar(Vehicle):
+
+    def __init__(self, company, model, year, color, max_speed):
+        super().__init__(company, model, year, color)
+        self.max_speed = max_speed
+        self.size = 1
