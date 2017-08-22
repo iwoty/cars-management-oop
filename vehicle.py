d3dd5f791383b619ff64b49aad390227c32497f8
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:36:24 +0200

    Add contructor of (parent) class Vehicle

--- vehicle.py
+++ vehicle.py
@@ -1,2 +1,9 @@
 
 class Vehicle:
+
+    def __init__(self, company, model, year, color):
+        self.company = company
+        self.model = model
+        self.year = year
+        self.color = color
+        self.size = None
