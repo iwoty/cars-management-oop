08d5ab9498ba043937b9b83f7c78499a1dd3d2be
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:46:58 +0200

    Add Truck class contructor (with super method)

--- truck.py
+++ truck.py
@@ -3,3 +3,8 @@ from van import Van
 
 
 class Truck(Van):
+
+    def __init__(self, company, model, year, color, max_capacity, wheel_count):
+        super().__init__(company, model, year, color, max_capacity)
+        self.wheel_count = wheel_count
+        self.size = 3
