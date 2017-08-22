91bf19a742037a8e2a5a0d76d068309a1047638e
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:44:06 +0200

    Add Van class contructor (with super method)

--- van.py
+++ van.py
@@ -3,3 +3,8 @@ from vehicle import Vehicle
 
 
 class Van(Vehicle):
+
+    def __init__(self, company, model, year, color, max_capacity):
+        super().__init__(company, model, year, color)
+        self.max_capacity = max_capacity
+        self.size = 2
