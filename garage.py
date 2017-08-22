7b9ee5bce74e7311067c6e0afcf6436df4761203
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:49:14 +0200

    Create Garage class with its contructor (with super method)

--- garage.py
+++ garage.py
@@ -6,3 +6,11 @@ from vehicle import Vehicle
 from sportcar import Sportcar
 from van import Van
 from truck import Truck
+
+
+class Garage:
+
+    def __init__(self, space, address):
+        self.space = space
+        self.address = address
+        self.vehicles_list = []
