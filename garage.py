53b4bc6727c2c2b103402072bc4448a13f9705dd
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:54:43 +0200

    Add load_cars_from_csv method thats creates garage with given address and adds cars from csv to it

--- garage.py
+++ garage.py
@@ -34,3 +34,42 @@ class Garage:
             vehicles_list_str += '{}. {}\n'.format(i, vehicle.display_info())
             i += 1
         return 'Cars available in {}:\n{}'.format(self.address, vehicles_list_str)
+
+    @staticmethod
+    def load_cars_from_csv(address, csv_path):
+
+        with open(csv_path) as source:
+            vehicle_list = csv.DictReader(source, delimiter='	')
+
+            space = 0
+            for vehicle in vehicle_list:
+                if vehicle['type'] == 'sport':
+                    space += 1
+                elif vehicle['type'] == 'van':
+                    space += 2
+                elif vehicle['type'] == 'truck':
+                    space += 3
+
+            source.seek(0)
+            # sets the iterator to beginning of the input file - otherwise
+            # we cannot iterate one more time or we have to open file one more time
+
+            new_garage = Garage(space, address)
+
+            for vehicle in vehicle_list:
+                if vehicle['type'] == 'sport':
+                    new_vehicle = Sportcar(vehicle['company'], vehicle['model'], vehicle['year'],
+                                           vehicle['color'], vehicle['max_speed'])
+                    new_garage.add_car(new_vehicle)
+
+                elif vehicle['type'] == 'van':
+                    new_vehicle = Van(vehicle['company'], vehicle['model'], vehicle['year'],
+                                      vehicle['color'], vehicle['max_capacity'])
+                    new_garage.add_car(new_vehicle)
+
+                elif vehicle['type'] == 'truck':
+                    new_vehicle = Truck(vehicle['company'], vehicle['model'], vehicle['year'],
+                                        vehicle['color'], vehicle['max_capacity'], vehicle['wheel_count'])
+                    new_garage.add_car(new_vehicle)
+
+        return new_garage
