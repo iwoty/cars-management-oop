1bf91a461a1c7765190d37af7784a15f4ed705c3
Iwo <iwo.tyszkowski@gmail.com>
2017-08-22 17:42:17 +0200

    Add display_info method thats returns string with information about Sportcar instance

--- sportcar.py
+++ sportcar.py
@@ -8,3 +8,7 @@ class Sportcar(Vehicle):
         super().__init__(company, model, year, color)
         self.max_speed = max_speed
         self.size = 1
+
+    def display_info(self):
+        return '{} {} {}, from {}, max speed: {} km/h'.format(self.color, self.company,
+                                                              self.model, self.year, self.max_speed)
