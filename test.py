30f7cfbeb2a2588bf096f8e9f591d50978a3da44
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2017-08-22 13:26:06 +0200

    Add car size testing

--- test.py
+++ test.py
@@ -27,55 +27,79 @@ class TestCheckpointExcersise(unittest.TestCase):
     def test_01_sportcar_company(self):
         self.assertEqual("Lamborghini", self.get_sportcar().company)
 
-    def test_02_sportcar_max_speed(self):
+    def test_02_sportcar_model(self):
+        self.assertEqual("Diablo", self.get_sportcar().model)
+
+    def test_03_sportcar_year(self):
+        self.assertEqual(2000, self.get_sportcar().year)
+
+    def test_04_sportcar_color(self):
+        self.assertEqual("bloody red", self.get_sportcar().color)
+
+    def test_05_sportcar_max_speed(self):
         self.assertEqual(320, self.get_sportcar().max_speed)
 
-    def test_03_sportcar_displayed_info(self):
+    def test_06_sportcar_displayed_info(self):
         msg = "bloody red Lamborghini Diablo, from 2000, max speed: 320 km/h"
         self.assertEqual(self.get_sportcar().display_info(), msg)
 
-    def test_04_van_model(self):
+    def test_07_sportcar_size(self):
+        self.assertEqual(1, self.get_sportcar().size)
+
+    def test_08_van_model(self):
         self.assertEqual("WTF1", self.get_van().model)
 
-    def test_05_van_max_capacity(self):
+    def test_09_van_max_capacity(self):
         self.assertEqual(1500, self.get_van().max_capacity)
 
-    def test_06_van_displayed_info(self):
+    def test_10_van_size(self):
+        self.assertEqual(2, self.get_van().size)
+
+    def test_11_van_displayed_info(self):
         msg = "yellow GMC WTF1, from 2016, max capacity: 1500 kg"
         self.assertEqual(self.get_van().display_info(), msg)
 
-    def test_07_truck_displayed_info(self):
+    def test_12_truck_model(self):
+        self.assertEqual("M45", self.get_truck().model)
+
+    def test_13_truck_wheels(self):
+        self.assertEqual(8, self.get_truck().wheel_count)
+
+    def test_14_truck_size(self):
+        self.assertEqual(3, self.get_truck().size)
+
+    def test_15_truck_displayed_info(self):
         msg = "8-wheels black Mercedes M45, from 1997, max capacity: 8000 kg"
         self.assertEqual(self.get_truck().display_info(), msg)
 
-    def test_08_garage_location(self):
+    def test_16_garage_location(self):
         self.assertEqual("New York", self.get_garage1().address)
 
-    def test_09_garage_initial_capacity(self):
+    def test_17_garage_initial_capacity(self):
         self.assertEqual(2, self.get_garage1().space_left())
 
-    def test_10_garage_type_error(self):
+    def test_18_garage_type_error(self):
         garage = self.get_garage1()
         with self.assertRaises(TypeError, msg="Test dupy się nie powiódł ;("):
             garage.add_car("dupa")
 
-    def test_11_garage_space_usage_vehicle_fits(self):
+    def test_19_garage_space_usage_vehicle_fits(self):
         garage = self.get_garage1()
         garage.add_car(self.get_sportcar())
         self.assertEqual(1, garage.space_left())
 
-    def test_12_garage_space_two_vehicles(self):
+    def test_20_garage_space_two_vehicles(self):
         garage2 = self.get_garage2()
         garage2.add_car(self.get_truck())
         garage2.add_car(self.get_truck())
         self.assertEqual(0, garage2.space_left())
 
-    def test_13_garage_space_usage_vehicle_doesnt_fit(self):
+    def test_21_garage_space_usage_vehicle_doesnt_fit(self):
         garage = self.get_garage1()
         with self.assertRaises(OverflowError, msg="This should not fit in here"):
             garage.add_car(self.get_truck())
 
-    def test_14_garage_display_vehicles(self):
+    def test_22_garage_display_vehicles(self):
         expected_string = "Cars available in San Escobar:\n"\
                           "1. bloody red Lamborghini Diablo, from 2000, max speed: 320 km/h\n" \
                           "2. yellow GMC WTF1, from 2016, max capacity: 1500 kg\n" \
